2653d5201487de0b4037eb97903b5724cb77a056
MigasZ <145591269+MigasZ@users.noreply.github.com>
2023-11-23 08:53:10 -0300

    Add files via upload

--- MacroAuto.py
+++ MacroAuto.py
@@ -3,7 +3,7 @@ import time
 import keyboard
 
 def pressiona_teclas():
-    # Simula a combinação de teclas "seta para esquerda" e "enter"
+    # Simula a combinação de teclas "seta para a esquerda" e "enter"
     pyautogui.press('right')
     time.sleep(0.2)
     pyautogui.press('enter')
@@ -12,21 +12,20 @@ def executa_macro():
     # Inicia o loop para execução contínua do macro
     while True:
         pressiona_teclas()
-        time.sleep(5)  # Intervalo de 5 segundos entre cada execução
+        time.sleep(24)  # Intervalo de 5 segundos entre cada execução
 
 def ativa_desativa_macro():
-    # Função para ativar/desativar o macro com o atalho Fn + P
-    atalho_ativar = 'f12'  # Substitua pelo seu atalho desejado (Fn + P)
-    atalho_desativar = 'esc'  # Substitua pelo seu atalho desejado para desativar
+    # Função para ativar/desativar o macro com o atalho F12
+    atalho_ativar = 'f12'  # Substitua pelo seu atalho desejado (F12)
 
     ativar = False
 
     while True:
         if keyboard.is_pressed(atalho_ativar) and not ativar:
-            ativa_desativa_macro = True
+            ativar = True
             executa_macro()
-        elif keyboard.is_pressed(atalho_desativar) and ativar:
-            ativa_desativa_macro = False
+        elif keyboard.is_pressed(atalho_ativar) and ativar:
+            ativar = False
             break
 
 # Chama a função para ativar/desativar o macro
